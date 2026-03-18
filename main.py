5177b6bc8f6c8eac3be178d69919a7e2895f2dff
lushbo <alexvgarcia5@gmail.com>
2026-03-17 22:57:28 -0700

    Refactor variable names for consistency in main.py

--- main.py
+++ main.py
@@ -5,40 +5,45 @@ print("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 
 
 input_level = input("\nPlease select the difficulty level:\n1. Easy (10 chances)\n2. Medium (5 chances)\n3. Hard (3 chances)\n\nEnter your choice:")
 
-Chances = 0
-Easy = Chances + 10
-Medium = Chances + 5
-Hard =  Chances + 3
+chances = 0
+easy = chances + 10
+medium = chances + 5
+hard =  chances + 3
 
 if input_level == "1":
-    Chances = 10
+    chances = 10
+    difficulty = easy
 elif input_level == "2":
-    Chances = 5
+    chances = 5
+    difficulty = medium
 elif input_level == "3":
-    Chances = 3
+    chances = 3
+    difficulty = hard
 else:
     print("\nLetra no reconocida\nIntenta de nuevo")
 
 if input_level == "1":
-    Difficulty = Easy
+    difficulty = easy
 elif input_level == "2":
-    Difficulty = Medium
+    difficulty = medium
 elif input_level == "3":
-    Difficulty = Hard
-print(f"\nDifficulty : {Difficulty}\nIntentos: {Chances}\n")
+    difficulty = hard
+print(f"\nDifficulty : {difficulty}\nIntentos: {chances}\n")
 
-while Chances > 0:
+while chances > 0:
     guess = int(input("Elige un numero del 1 al 100:"))
-    Chances -=1
+    chances -=1
 
     if guess == number:
         print("Felicidades, Acertaste!!!")
         break
     elif guess > number:
-        print(f"\nEl numero es menor, Te quedan {Chances} intentos!\n")
+        print(f"\nEl numero es menor, Te quedan {chances} intentos!\n")
     else:
-        print(f"\nEl numero es mayor. Te quedan {Chances} intentos!\n")
+        print(f"\nEl numero es mayor. Te quedan {chances} intentos!\n")
 
-if Chances == 0 and guess != number:
+if chances == 0 and guess != number:
     print(f"\nPerdiste, el numero era {number}")
+
+
 
